95bf4cea254b6c1d999a42b65e8c7361fbe99e15
Hachem Zarrad <hachemzarrad@gmail.com>
2020-09-27 03:06:24 +0100

    Second Assignment

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
@@ -37,6 +38,12 @@ class Main extends Component {
       );
     };
 
+    const AboutPage = () => {
+      return(
+          <About leaders = {this.state.leaders}></About>
+      );
+    }
+
     const HomePage = () => {
       return(
         <Home 
@@ -55,6 +62,7 @@ class Main extends Component {
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={Contact} />
+              <Route exact path='/aboutus' component= {AboutPage} />
               <Redirect to="/home" />
               
           </Switch>
